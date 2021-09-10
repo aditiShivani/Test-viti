fa5b156f00f6157ab03cf0d677714fe45848f771
Aditi <shivaniaditi17521@gmail.com>
2021-09-10 21:02:32 +0530

    Initial commit

---
+++ components/Footer/footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+        Made by{" "}
+        <a
+          href="https://www.linkedin.com/in/aditi-shivani-801b421ab/"
+          style={{ cursor: "pointer" }}
+        >
+          Aditi shivani
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
---
+++ components/Header/Header.js
@@ -0,0 +1,27 @@
+import "./Header.css";
+import { Link } from "react-router-dom";
+
+
+const Header = () => {
+    return (
+        <div>
+
+            <Link to="/" className="title">
+            make it or break it!
+            </Link>
+
+            <hr className="divider" />
+
+
+
+
+
+
+
+
+        </div>
+    );
+
+};
+
+export default Header;
